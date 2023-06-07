ee28fc5f8fa71d9fc9c96f34aec78909a0a0092d
Hao Y. Li <hao@levelshealth.com>
2023-06-07 12:50:58 -0700

    fix: code cleanup

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse, HttpResponseBadRequest
 from sqlalchemy import create_engine, inspect
-import sqlalchemy.exc
 from .models import TableMetadata, ColumnMetadata
 
 # Create your views here.
@@ -12,24 +11,26 @@ def table_metadata(request):
     # Get the table name from the GET request
     table_name = request.GET.get("table_name")
 
+    content_type = 'application/json'
+
     # If no database connection string was provided, return a 400 (Bad Request) response
     if not db_string:
-        return HttpResponseBadRequest(content='{"error": "No database connection string provided."}', content_type='application/json')
+        return HttpResponseBadRequest(content='{"error": "No database connection string provided."}', content_type=content_type)
 
     # If no table name was provided, return a 400 (Bad Request) response
     if not table_name:
-        return HttpResponseBadRequest(content='{"error": "No table name provided."}', content_type='application/json')
+        return HttpResponseBadRequest(content='{"error": "No table name provided."}', content_type=content_type)
 
     # Try to connect to the database and get the table metadata
     try:
         engine = create_engine(db_string)
         inspector = inspect(engine)
     except Exception as e:
-        return HttpResponseBadRequest(content='{"error": "Could not connect to database. Error: ' + str(e) + '"}', content_type='application/json')
+        return HttpResponseBadRequest(content='{"error": "Could not connect to database. Error: ' + str(e) + '"}', content_type=content_type)
 
     # If the table does not exist in the database, return a 400 (Bad Request) response
     if table_name not in inspector.get_table_names():
-        return HttpResponseBadRequest(content='{"error": "Table does not exist in the database."}', content_type='application/json')
+        return HttpResponseBadRequest(content='{"error": "Table does not exist in the database."}', content_type=content_type)
 
     # Raw SQL for getting the number of rows in the table
     select_sql_raw = f"SELECT COUNT(*) FROM {table_name}"
@@ -43,7 +44,7 @@ def table_metadata(request):
         columns = inspector.get_columns(table_name)
         num_rows = engine.execute(select_sql_raw).fetchone()[0]
     except Exception as e:
-        return HttpResponseBadRequest(content='{"error": "Could not retrieve table metadata. Error: ' + str(e) + '"}', content_type='application/json')
+        return HttpResponseBadRequest(content='{"error": "Could not retrieve table metadata. Error: ' + str(e) + '"}', content_type=content_type)
 
     # Create the TableMetadata object
     col_metadata = [ColumnMetadata(col['name'], col['type']) for col in columns]
